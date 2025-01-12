0eb9eef2d1709429ec6247b9eccbbc0d3087b179
Vishwesh-Bhilare <bhilarevishwesh@gmail.com>
2025-01-12 19:01:12 +0530

    Update extract_m3u.py

--- extract_m3u.py
+++ extract_m3u.py
@@ -1,1 +1,22 @@
+import os
 
+def create_m3u_playlist(music_directory):
+    """Creates an M3U playlist for all .flac files in a given directory.
+
+    Args:
+        music_directory: The path to the root music directory.
+    """
+
+    m3u_file_path = os.path.join(music_directory, "music_playlist.m3u")
+
+    with open(m3u_file_path, "w") as m3u_file:
+        for root, _, files in os.walk(music_directory):
+            for file in files:
+                if file.endswith(".flac"):
+                    relative_path = os.path.relpath(os.path.join(root, file), music_directory)
+                    m3u_file.write(relative_path + "\n")
+
+# Replace 'X:\Music' with the actual path to your music directory
+# Create separate m3u files for each playlist
+music_directory = r"X:\Music"
+create_m3u_playlist(music_directory)
